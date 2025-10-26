8219c1ff8fa460dc2bc7037c2dea2c5dc36bfc78
gayathrilakshmi1410 <gayathrilakshmi1410@gmail.com>
2025-10-26 23:49:18 +0530

    updated base URL

--- client/src/utils/services.js
+++ client/src/utils/services.js
@@ -1,4 +1,5 @@
-export const baseUrl="http://localhost:5000/api";
+// export const baseUrl="http://localhost:5000/api";
+export const baseUrl="https://chat-app-1-upb6.onrender.com/api";
 
 export const postRequest=async(url,body)=>{
    
